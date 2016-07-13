031577dec1af9d044eee465c4a3d3832988de446
av-elier <adelier1111@gmail.com>
2016-07-13 23:53:40 +0300

    Remove unused config and comment

--- src/main/java/ru/av_elier/money/txt/OAuthWebController.java
+++ src/main/java/ru/av_elier/money/txt/OAuthWebController.java
@@ -30,7 +30,6 @@ public class OAuthWebController {
         // Get your app key and secret from the Dropbox developers website.
         final String APP_KEY = System.getenv("DROPBOX_APP_KEY");
         final String APP_SECRET = System.getenv("DROPBOX_APP_SECRET");
-        final String REDIRECT_URL = System.getenv("DROPBOX_APP_REDIRECT");
 
         DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
 
@@ -46,34 +45,4 @@ public class OAuthWebController {
         String accessToken = authFinish.accessToken;
         return accessToken;
     }
-
-//    public void init() throws IOException, DbxException {
-////        System.out.println("1. Go to: " + authorizeUrl);
-////        System.out.println("2. Click \"Allow\" (you might have to log in first)");
-////        System.out.println("3. Copy the authorization code.");
-//        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-//
-//
-//    }
-//
-//    public void start() {
-//        port(Integer.valueOf(System.getenv("PORT")));
-//        get("/dropbox/", (req, res) -> {
-//            // save something
-//            Set<String> queryParams = req.queryParams();
-//            Map<String, String[]> parameterMap = new HashMap<String, String[]>(queryParams.size());
-//            for (String param : queryParams) {
-//                parameterMap.put(param, req.queryParamsValues(param));
-//            }
-//
-//
-//            // and return user to the chat
-//            res.redirect("https://telegram.me/MoneyTxtBot");
-//            return null;
-//        });
-//
-//    }
-//    public void stop() {
-//        stop();
-//    }
 }
